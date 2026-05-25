9d0fdd2252288a4af2ea42e49df76f048a74b886
Reilly Featherstone <contact@reillyfeatherstone.com>
2026-05-25 18:37:58 +0100

    get songs script

---
+++ src/content/index.ts
@@ -0,0 +1,31 @@
+type Song = {
+  rank: number
+  name: string
+  artist: string
+}
+
+export function extractChartData(): Song[] {
+  const songs: Song[] = []
+  const chartList = document.querySelector('.chart-list')
+
+  if (!chartList) return songs
+
+  const chartItems = chartList.querySelectorAll('.chart-item')
+
+  chartItems.forEach((item) => {
+    const songNameEl = item.querySelector(
+      '.chart-name span:not(.movement-icon)',
+    )
+    const artistEl = item.querySelector('.chart-artist span')
+
+    if (songNameEl || artistEl) {
+      songs.push({
+        rank: songs.length + 1,
+        name: songNameEl?.textContent?.trim() || '',
+        artist: artistEl?.textContent?.trim() || '',
+      })
+    }
+  })
+
+  return songs
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "vite"
-import react, { reactCompilerPreset } from "@vitejs/plugin-react"
-import babel from "@rolldown/plugin-babel"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        content: 'src/content/index.ts',
+      },
+      output: {
+        entryFileNames: '[name].js',
+      },
+    },
+  },
   plugins: [
     react(),
     tailwindcss(),
